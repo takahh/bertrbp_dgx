1f84b10537f365429a3c1117871399002442475e
Takayuki Kimura <d@wakou.cc>
2024-06-20 15:15:25 +0900

    sss

--- scripts/train_and_test.sh
+++ scripts/train_and_test.sh
@@ -15,7 +15,7 @@ OUTPUT_PATH=$ORIG_PATH$RBP/finetuned_model/
 
 echo "######### running training ##########"
 python $PYTHON_PATH --no_cuda --overwrite_cache --model_type dna --tokenizer_name dna$KMER --model_name_or_path $2 --task_name dnaprom --data_dir $DATA_PATH --output_dir $OUTPUT_PATH --do_train --max_seq_length 101 --per_gpu_eval_batch_size 32 --per_gpu_train_batch_size 32 --learning_rate 2e-4 --num_train_epochs 1 --logging_steps 200 --warmup_percent 0.1 --hidden_dropout_prob 0.1 --overwrite_output_dir --weight_decay 0.01 --n_process 8
-
+# python3 $PYTHON_PATH --model_type dna --tokenizer_name dna$KMER --model_name_or_path $MODEL_PATH --task_name dnaprom --data_dir $DATA_PATH --output_dir $OUTPUT_PATH --do_train --max_seq_length 101 --per_gpu_eval_batch_size 32 --per_gpu_train_batch_size 32 --learning_rate 2e-4 --num_train_epochs 3 --logging_steps 200 --warmup_percent 0.1 --hidden_dropout_prob 0.1 --overwrite_output_dir --weight_decay 0.01 --n_process 8
 DATA_PATH=$ORIG_PATH$RBP/test_sample_finetune/
 MODEL_PATH=$ORIG_PATH$RBP/finetuned_model/
 PREDICT_PATH=$ORIG_PATH$RBP/finetuned_model/

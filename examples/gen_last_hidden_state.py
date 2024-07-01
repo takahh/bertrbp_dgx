c30317e5ca11625a13771f56286dcbee6a064c3a
Takayuki Kimura <d@wakou.cc>
2024-07-02 07:42:25 +0900

    sss

--- examples/gen_last_hidden_state.py
+++ examples/gen_last_hidden_state.py
@@ -6,6 +6,7 @@
 # import
 # -------------------------------------------------------------------
 import sys
+import os
 sys.path.append(os.path.join(os.path.dirname(__file__), '../attention_analysis'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../motif'))
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
